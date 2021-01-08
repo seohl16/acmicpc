4955b6860b3e18bcc51262bb5394cfcc6d79b231
42맥북 <selimmac@42maegbug-ui-MacBookPro.local>
2021-01-09 02:21:15 +0900

    1930

---
+++ 1930SuperSum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int bucket[15][15];
+
+void init_bucket(void)
+{
+	for (int k = 0; k < 15; k++)
+	{
+		for (int n = 0; n < 15; n++)
+		{
+			if (k == 0)
+				bucket[k][n] = n;
+			else
+				bucket[k][n] = 0;
+		}
+	}
+}
+
+int supersum(int k, int n)
+{
+	int sum;
+
+	sum = 0;
+	if (k == 0 || bucket[k][n] != 0)
+		return (bucket[k][n]);
+	for (int i = 1; i <= n; i++)
+	{
+		sum += supersum(k - 1, i);
+	}
+	if (bucket[k][n] == 0)
+		bucket[k][n] = sum;
+	return (sum);
+}
+
+int main(void)
+{
+	int k;
+	int n;
+	init_bucket();
+	while (scanf("%d %d", &k, &n) != EOF)
+		printf("%d\n", supersum(k, n));
+}
