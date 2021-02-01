04ffb101e88afbb238969b4197b549b5ac5f6a73
42맥북 <selim@student.42seoul.kr>
2021-02-01 12:18:28 +0900

    20210201

---
+++ BOJ/dynamicprog다이나믹/1890jump.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int arr[101][101];
+int dp[101][101];
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+			cin >> arr[i][j];
+	}
+	dp[1][1] = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == 1 && j == 1)
+				continue;
+			for (int k = 1; k < j; k++)
+			{
+				if (k + arr[i][k] == j)
+				{
+					dp[i][j] += dp[i][k];
+				}
+			}
+			for (int k = 1; k < i; k++)
+			{
+				if (k + arr[k][j] == i)
+					dp[i][j] += dp[k][j];
+			}
+		}
+	}
+	cout << dp[n][n] << "\n";
+}
