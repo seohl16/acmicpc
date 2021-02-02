0c7dd7307bb9fb4515ce28f6f20713e196b165e4
42맥북 <selim@student.42seoul.kr>
2021-02-02 11:26:11 +0900

    20210202

---
+++ BOJ/dynamicprog다이나믹/11659partsum4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[100001];
+int partsum[100001];
+
+
+int main(void)
+{
+	int n, m;
+	cin >> n >> m;
+	int a, b;
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &arr[i]);
+	for (int i = 1; i <= n; i++)
+	{
+		partsum[i] = partsum[i - 1] + arr[i];
+	}
+	for (int j = 1; j <= m; j++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", partsum[b] - partsum[a - 1]);
+		//cout << partsum[b] - partsum[a - 1] << "\n";
+	}
+}
+
+//분명 같은 코드인데 cout cin을 쓰면 시간초과 난다
